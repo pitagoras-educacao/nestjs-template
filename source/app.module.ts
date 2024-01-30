b130b5c065a1f36ebb4d33d77a138fa140cf5f9e
Letícia Aguiar <letsaguiar@yandex.com>
2024-01-30 16:47:52 -0300

    feat: install typeorm

--- source/app.module.ts
+++ source/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            url: process.env['DATABASE_URL'] || 'postgres://postgres:postgres@localhost:5432/postgres',
+            entities: [],
+            synchronize: true,
+          }),
+      
+    ]
+})
 export class AppModule {}
