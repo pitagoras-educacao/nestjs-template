c1581475e514da8c6f1271a4baf2c89a60396ce7
Letícia Aguiar <letsaguiar@yandex.com>
2024-02-08 12:26:12 -0300

    feat: setup validation pipe

--- source/main.ts
+++ source/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -7,6 +7,7 @@ async function main(): Promise<void>
 {
     const app = await NestFactory.create(AppModule);
 
+    setupValidationPipe(app);
     setupSwagger(app);
 
     await app.listen(3000);
@@ -22,6 +23,15 @@ function setupSwagger(app: INestApplication): void
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, document);
+}
+
+function setupValidationPipe(app: INestApplication): void
+{
+    app.useGlobalPipes(new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+    }));
 }
 
 main();
