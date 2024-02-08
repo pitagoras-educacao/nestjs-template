594bcd68a9ebce8df33ff1db18caae9c04d28e41
Letícia Aguiar <letsaguiar@yandex.com>
2024-02-08 12:24:17 -0300

    feat: install swagger

--- source/main.ts
+++ source/main.ts
@@ -1,10 +1,27 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function main(): Promise<void>
 {
     const app = await NestFactory.create(AppModule);
+
+    setupSwagger(app);
+
     await app.listen(3000);
+}
+
+function setupSwagger(app: INestApplication): void
+{
+    const config = new DocumentBuilder()
+        .setTitle('NestJS API')
+        .setDescription('NestJS API description')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
 }
 
 main();
